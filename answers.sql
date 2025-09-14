f45a301de39b7715903623e5be29bfe7daedba19
godwin <kipkoechgodwin523@gmail.com>
2025-09-14 10:54:34 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,22 @@
+-- Question 1 🧑‍🎓
+-- Create table student
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    age INT
+);
+
+-- Question 2 ➕
+-- Insert at least 3 records
+INSERT INTO student (id, fullName, age) 
+VALUES 
+(1, 'Alice Johnson', 18),
+(2, 'Brian Smith', 19),
+(3, 'Clara Davis', 21);
+
+-- Question 3 🔄
+-- Update age of student with ID 2 to 20
+UPDATE student
+SET age = 20
+WHERE id = 2;
 
